f1359d976cbcd2820ff4e98a5dc16704295c1f20
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-21 16:23:29 +1300

    creates the schema for the db and minor changes to jobs

---
+++ db/01_database_schema.sql
@@ -0,0 +1,15 @@
+BEGIN TRANSACTION
+
+CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    clientName TEXT NOT NULL,
+    location TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+    clientPhoneNumber TEXT NOT NULL,
+    jobStatus TEXT NOT NULL,
+    userId INTEGER NOT NULL,
+    JobDate TEXT NOT NULL,
+);
