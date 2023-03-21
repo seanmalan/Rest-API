608bdeff4cc455968d337d02529ac50ed1254ecf
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-21 14:15:05 +1300

    setting up the server to run and have a db attasched to it

---
+++ api/app.js
@@ -0,0 +1,20 @@
+// const db = require("./db");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/test", async (req, res, next) => {
+  try {
+    res.send("Hello World!!!").status(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = app;
