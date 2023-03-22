84cb2b50aa766a0aef327e3bd2e73e3446636c65
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-22 18:51:17 +1300

    creates a test for the get by id route

--- api/routes/jobs.test.js
+++ api/routes/jobs.test.js
@@ -1,3 +1,4 @@
+const { doesNotMatch } = require('assert');
 const request = require('supertest');
 const app = require('../app');
 const { getAllJobs, getJobById } = require('../routes/jobs.repository');
@@ -39,6 +40,7 @@ describe('GET all jobs', () => {
       expect(response.body).toEqual(fakeJobs);
       expect(response.statusCode).toBe(200);
       expect(getJobById).not.toHaveBeenCalled();
+      
   });
 
   it('should return a 404 if wrong url', async () => {
@@ -83,4 +85,42 @@ describe('GET all jobs', () => {
       expect(getAllJobs).not.toHaveBeenCalled();
     });
 
+});
+
+
+describe("GET a job by id", () => {
+  test("should return a single job", async () => {
+    const fakeJob = {
+      id: 12,
+      title: "build a big fence",
+      clientname: "John Smith",
+      location: "123 Main St",
+      description: "build a fence",
+      created_at: "2023-03-21T01:37:26.327Z",
+      updated_at: "2023-03-22T01:37:26.327Z",
+      clientphonenumber: "1234567890",
+      jobstatus: "active",
+      userid: 31,
+      jobdate: "2019-01-01",
+    };
+    getJobById.mockImplementation(() => fakeJob);
+
+    const response = await request(app).get('/jobs/12');
+
+    
+    expect(response.body).toEqual(fakeJob);
+    expect(response.statusCode).toBe(200);
+    expect(getAllJobs).not.toHaveBeenCalled();
+  });
+
+  test("should return a 404 if wrong url", async () => {
+    const noJob = {};
+    getJobById.mockImplementation(() => noJob);
+
+    const response = await request(app).get("/job/1awd");
+
+    expect(response.body).toEqual(noJob);
+    expect(response.statusCode).toBe(404);
+    expect(getAllJobs).not.toHaveBeenCalled();
+  });
 });
