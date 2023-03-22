7cb0cfd5be2373fed60bdcc959b30b74853bd7d9
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-23 12:04:40 +1300

    creates the react app and render.yml file

--- api/db.js
+++ api/db.js
@@ -1,15 +1,13 @@
-const { Pool } = require('pg');
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: "db",
-    user: "postgres",
-    password: "password",
-    database: "postgres",
-    port: "5432",
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  });  
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL
+  ? {
+      rejectUnauthorized: false,
+    }
+  : false,
+});
 
 module.exports = {
   query: (text, params, callback) => {
@@ -19,3 +17,32 @@ module.exports = {
     pool.end();
   },
 };
+
+module.exports = pool;
+
+
+
+
+
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     host: "db",
+//     user: "postgres",
+//     password: "password",
+//     database: "postgres",
+//     port: "5432",
+//     max: 20,
+//     idleTimeoutMillis: 30000,
+//     connectionTimeoutMillis: 2000,
+//   });  
+
+// module.exports = {
+//   query: (text, params, callback) => {
+//     return pool.query(text, params, callback);
+//   },
+//   end: () => {
+//     pool.end();
+//   },
+// };
