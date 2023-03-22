1c90dc0d279e297b4e081fbb3d34a66811955cdb
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-22 20:24:59 +1300

    added celebrate validation

--- api/routes/jobs.js
+++ api/routes/jobs.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const jobsRepository = require("./jobs.repository");
+const { celebrate, Joi, errors, Segments } = require("celebrate");
+
+
+
+
+
+const acceptedJobStatuses = [
+  "Pending",
+  "Active",
+  "Completed",
+  "Invoiced",
+  "Cancelled",
+  "To Price",
+];
+
+
+
+router.post("/",
+celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    clientname: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+    clientphonenumber: Joi.string().required(),
+    jobstatus: Joi.string().valid(...acceptedJobStatuses).required(),
+    userid: Joi.number().required(),
+    jobdate: Joi.date().required(),
+  }),
+}), async (req, res, next) => {
+  try {
+    const job = await jobsRepository.createJob(req.body);
+    res.send(job).status(201);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+
 
 router.get("/", async (req, res, next) => {
   try {
@@ -21,33 +61,10 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const job = await jobsRepository.createJob(req.body);
-    res.send(job).status(201);
-  } catch (error) {
-    next(error);
-  }
-});
 
 
 
 
-// have a route with a variable for the job status
-// router.get("/", async (req, res, next) => {
-//   try {
-//     // get the status from the request
-//     const status = req.params.status;
-//    console.log(status);
-//   // call the repo with the status
-//   // send the response
-//     const jobs = await jobsRepository.getJobsByStatus(req.params.status);
-//     res.send(jobs).status(200);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-
+router.use(errors());
 
 module.exports = router;
