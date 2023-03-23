700561dd16cd462631207a17183b9bfee524b115
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-23 15:13:17 +1300

    retrieves the job list

---
+++ client/src/components/JobList.js
@@ -0,0 +1,87 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./JobList.css";
+// import { formatDate } from "../util/formatDate";
+
+const JobList = () => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isNotFound, setIsNotFound] = useState(false);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const response = await fetch(`https://api-juaz.onrender.com/jobs`);
+
+      if (response.ok === false) {
+        setIsNotFound(true);
+        return;
+      }
+
+      const data = await response.json();
+      setJobs(data);
+      setLoading(false);
+    };
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (jobs.length < 1) {
+    setIsNotFound(true);
+  }
+
+  console.log(jobs);
+  if (isNotFound) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <h1 className="container-title">Job List</h1>
+        <ul className="JobList">
+          {jobs.map((job) => {
+            return (
+              <div key={job.id} className="job">
+                <li>
+                  <h2>{job.title}</h2>
+                  <p>
+                    Customer Name: <span>{job.clientname}</span>
+                  </p>
+                  <p>
+                    Site Address: <span>{job.location}</span>
+                  </p>
+                  <div className="job-description">
+                    <p>Job Description:</p>
+                    <p>{job.description}</p>
+                  </div>
+                  <div>
+                    <p>Client Phone Number:</p>
+                    <p>{job.clientphonenumber}</p>
+                  </div>
+                  <p>
+                    Job Status: <span>{job.jobstatus}</span>
+                  </p>
+                  <div>
+                    <p>Job Notes:</p>
+                    <p>{job.jobnotes}</p>
+                  </div>
+                  <div>
+                    <p>Job Date:</p>
+                    <p>{job.jobdate}</p>
+                  </div>
+
+                  <Link to={`jobs/${job.id}`}> Check out Job &rarr;</Link>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default JobList;
