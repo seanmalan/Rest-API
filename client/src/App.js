72080f141d55b4bf05911dfb295ac8efce2c8dce
Sean Malan <sean.malan@developersinstitute.co.nz>
2023-03-23 15:13:08 +1300

    adds header to the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import JobList from './components/JobList';
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
+      
+      {/* <header className="App-header">
         <h1> Hello from the client side</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +24,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+<Header />
+      <Routes>
+        <Route path="/" element={<JobList />} />
+      </Routes>
     </div>
   );
 }
